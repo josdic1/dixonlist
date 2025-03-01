29660adbbd28151eb1d0781db1d7283925a31552
josdic1 <joshuadicker@gmail.com>
2025-03-01 18:46:03 -0500

    Added /tasks route

--- dixonlist-api/server.js
+++ dixonlist-api/server.js
@@ -12,5 +12,10 @@ app.get("/", (req, res) => {
    res.send("DixonList API is running...");
 });
 
+app.get("/tasks", (req, res) => {
+   res.json([{ id: 1, task: "Test Task" }]);
+});
+
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
